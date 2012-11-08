fd124445f7507bb50a5f4211f69ee636e177ebd4
Nikita Gnotovs <nxxn@inbox.lv>
2012-11-08 15:31:03 +0200

    ;

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Gnotov::Application.routes.draw do
 
     root :to => "homes#index"
 
-    match '/' => 'homes#index', :as => 'homes', :via => :get
-    match '/' => 'homes#create', :as => 'homes', :via => :post
+    match '' => 'homes#index', :as => 'message', :via => :get
+    match '' => 'homes#create', :as => 'message', :via => :post
 
   end
 end
